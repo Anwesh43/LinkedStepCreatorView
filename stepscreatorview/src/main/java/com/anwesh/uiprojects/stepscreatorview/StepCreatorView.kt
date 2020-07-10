7b2456e2fe72d4bc42b329aa27b7e74c48af5dc2
Anwesh43 <anweshthecool0@gmail.com>
2020-07-10 06:25:30 +0530

    adding renderer in view

--- stepscreatorview/src/main/java/com/anwesh/uiprojects/stepscreatorview/StepCreatorView.kt
+++ stepscreatorview/src/main/java/com/anwesh/uiprojects/stepscreatorview/StepCreatorView.kt
@@ -56,14 +56,16 @@ fun Canvas.drawSCNode(i : Int, scale : Float, paint : Paint) {
 
 class StepCreatorView(ctx : Context) : View(ctx) {
 
-    override fun onDraw(canvas : Canvas) {
+    private val renderer : Renderer = Renderer(this)
 
+    override fun onDraw(canvas : Canvas) {
+        renderer.render(canvas)
     }
 
     override fun onTouchEvent(event : MotionEvent) : Boolean {
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
-
+                renderer.handleTap()
             }
         }
         return true
@@ -187,7 +189,7 @@ class StepCreatorView(ctx : Context) : View(ctx) {
         private val sc : StepCreator = StepCreator(0)
         private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
 
-        fun render(canvas : Canvas, paint : Paint) {
+        fun render(canvas : Canvas) {
             canvas.drawColor(backColor)
             sc.draw(canvas, paint)
             animator.animate {
