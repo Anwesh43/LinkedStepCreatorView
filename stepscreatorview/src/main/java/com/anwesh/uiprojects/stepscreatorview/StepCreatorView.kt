0ae23a860028a3dbd102eb5bafa62506e803ca80
Anwesh43 <anweshthecool0@gmail.com>
2020-07-10 06:05:36 +0530

    adding SCNode class

--- stepscreatorview/src/main/java/com/anwesh/uiprojects/stepscreatorview/StepCreatorView.kt
+++ stepscreatorview/src/main/java/com/anwesh/uiprojects/stepscreatorview/StepCreatorView.kt
@@ -116,4 +116,45 @@ class StepCreatorView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class SCNode(var i : Int, val state : State = State()) {
+
+        private var next : SCNode? = null
+        private var prev : SCNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = SCNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawSCNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : SCNode {
+            var curr : SCNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
