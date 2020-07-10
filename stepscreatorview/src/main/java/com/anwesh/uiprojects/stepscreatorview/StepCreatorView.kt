21599332d8ddfe56b45c3bb96b6ab24dcaff0df5
Anwesh43 <anweshthecool0@gmail.com>
2020-07-10 05:51:17 +0530

    adding Canvas extension method

--- stepscreatorview/src/main/java/com/anwesh/uiprojects/stepscreatorview/StepCreatorView.kt
+++ stepscreatorview/src/main/java/com/anwesh/uiprojects/stepscreatorview/StepCreatorView.kt
@@ -24,3 +24,32 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawStepLine(i : Int, scale : Float, w : Float, h : Float, paint : Paint) {
+    val wGap : Float = w / (lines)
+    val hGap : Float = h / (lines)
+    val sf : Float = scale.sinify()
+    val sf1 : Float = sf.divideScale(0, (parts + 1)).divideScale(i, lines)
+    val sf2 : Float = sf.divideScale(1, (parts + 1)).divideScale(i, lines)
+    val sf3 : Float = sf.divideScale(2, (parts + 1)).divideScale(i, lines)
+    save()
+    translate(wGap * i, hGap * (i + 1) * sf2)
+    drawLine(0f, 0f, wGap * sf1, 0f, paint)
+    drawLine(wGap, -hGap * (i + 1) * sf3, 0f, 0f, paint)
+    restore()
+}
+
+fun Canvas.drawStepLines(scale : Float, w : Float, h : Float, paint : Paint) {
+    for (j in 0..(lines - 1)) {
+        drawStepLine(j, scale, w, h, paint)
+    }
+}
+
+fun Canvas.drawSCNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawStepLines(scale, w, h, paint)
+}
